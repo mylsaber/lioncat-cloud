944e57fcbbf5f7b243a0c9d513e24fc5475bcfb2
mylsaber <mylsaber@163.com>
2023-12-02 23:17:56 +0800

    登录controller修改

--- lioncat-module/lioncat-console-app/src/main/java/com/mylsaber/console/auth/contorller/AuthForBController.java
+++ lioncat-module/lioncat-console-app/src/main/java/com/mylsaber/console/auth/contorller/AuthForBController.java
@@ -6,6 +6,7 @@ import com.mylsaber.common.exception.BaseException;
 import com.mylsaber.common.pojo.CommonResult;
 import com.mylsaber.console.auth.service.AuthForBService;
 import com.mylsaber.console.auth.params.LoginRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,25 +16,32 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth/b")
+@Slf4j
 public class AuthForBController {
-    @Autowired
-    private AuthForBService authForBService;
 
-    @PostMapping("login")
-    public CommonResult<String> doLogin(@Validated @RequestBody LoginRequest loginRequest) {
-        try {
-            String token = authForBService.login(loginRequest.getUsername(), loginRequest.getPassword());
-            if (StrUtil.isBlank(token)) {
-                return CommonResult.getFail(null);
-            }
-            return CommonResult.getSuccess(token);
-        } catch (Exception e) {
-            throw new BaseException();
-        }
-    }
+  private AuthForBService authForBService;
 
-    @RequestMapping("isLogin")
-    public String isLogin() {
-        return "当前会话是否登录：" + StpUtil.isLogin();
+  @Autowired
+  public void setAuthForBService(AuthForBService authForBService) {
+    this.authForBService = authForBService;
+  }
+
+  @PostMapping("login")
+  public CommonResult<String> doLogin(@Validated @RequestBody LoginRequest loginRequest) {
+    try {
+      String token = authForBService.login(loginRequest.getUsername(), loginRequest.getPassword());
+      if (StrUtil.isBlank(token)) {
+        log.error("登录失败");
+        return CommonResult.getFail(null);
+      }
+      return CommonResult.getSuccess(token);
+    } catch (Exception e) {
+      throw new BaseException();
     }
+  }
+
+  @RequestMapping("isLogin")
+  public String isLogin() {
+    return "当前会话是否登录：" + StpUtil.isLogin();
+  }
 }
